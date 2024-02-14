5155082da0c3a5733e13a85fa5cfd58e2fdecbb3
bendcemb <bendcemb@gmail.com>
2024-02-14 16:30:37 +0700

    v2

---
+++ CD01/CD01/Controllers/RoomController.cs
@@ -0,0 +1,40 @@
+﻿using CD01.Data;
+using CD01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CD01.Controllers
+{
+    public class RoomController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public RoomController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RoomIndex()
+        {
+            return View();
+        }
+
+        public IActionResult RoomCreate() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult RoomCreate(Room obj)
+        { 
+            _db.Rooms.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("RoomIndex");
+        }
+    }
+}
---
+++ CD01/CD01/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using CD01.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CD01.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext>options):base(options) { 
+        
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Room> CMM_ROOMs { get; set; }
+    }
+}
